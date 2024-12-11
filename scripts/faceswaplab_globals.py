b33f8160eab1ab7d54145f014c3435efb89bbd31
Bragonborn <35249481+Bragonborn@users.noreply.github.com>
2024-12-11 20:22:26 +0100

    Fixed path error
    This path change fixes the following error (tested on my own setup  (installed via Stability Matrix on win11) ; 
    
    StabilityMatrix-win-x64\\Data\\Packages\\Stable Diffusion WebUI Forge\\extensions\\sd-webui-faceswaplab\\references\\woman.png'
    Traceback (most recent call last):
      File "D:\AI\StabilityMatrix-win-x64\Data\Packages\Stable Diffusion WebUI Forge\extensions\sd-webui-forge-faceswaplab\scripts\faceswaplab_swapping\face_checkpoints.py", line 76, in build_face_checkpoint_and_save
        reference_preview_img = Image.open(
      File "D:\AI\StabilityMatrix-win-x64\Data\Packages\Stable Diffusion WebUI Forge\venv\lib\site-packages\PIL\Image.py", line 3236, in open
        fp = builtins.open(filename, "rb")
    FileNotFoundError: [Errno 2] No such file or directory: 'D:\\AI\\StabilityMatrix-win-x64\\Data\\Packages\\Stable Diffusion WebUI Forge\\extensions\\sd-webui-faceswaplab\\references\\woman.png'
    
    D:\AI\StabilityMatrix-win-x64\Data\Packages\Stable Diffusion WebUI Forge\venv\lib\site-packages\insightface\utils\transform.py:68: FutureWarning: `rcond` parameter will change to the default of machine precision times ``max(M, N)`` where M and N are the input matrix dimensions.
    To use the future default and silence this warning we advise to pass `rcond=None`, to keep using the old, explicitly pass `rcond=-1`.
      P = np.linalg.lstsq(X_homo, Y)[0].T # Affine matrix. 3 x 4

--- scripts/faceswaplab_globals.py
+++ scripts/faceswaplab_globals.py
@@ -12,13 +12,13 @@ FACES_DIR = os.path.abspath(os.path.join(MODELS_DIR, "faces"))
 
 # Constructing the path for 'references' directory inside the 'extensions' and 'sd-webui-faceswaplab' directories, based on the base directory of scripts
 REFERENCE_PATH = os.path.join(
-    scripts.basedir(), "extensions", "sd-webui-faceswaplab", "references"
+    scripts.basedir(), "extensions", "sd-webui-forge-faceswaplab", "references"
 )
 
 # Defining the version flag for the application
 VERSION_FLAG: str = "v1.2.7"
 # Defining the path for 'sd-webui-faceswaplab' inside the 'extensions' directory
-EXTENSION_PATH = os.path.join("extensions", "sd-webui-faceswaplab")
+EXTENSION_PATH = os.path.join("extensions", "sd-webui-forge-faceswaplab")
 
 # Defining the expected SHA1 hash value for 'INSWAPPER'
 EXPECTED_INSWAPPER_SHA1 = "17a64851eaefd55ea597ee41e5c18409754244c5"
